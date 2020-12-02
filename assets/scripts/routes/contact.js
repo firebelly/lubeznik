f0eac8c40eda9ed51d7192b4a71054d996c0f2fc
poopsplat <matt.m.soria@gmail.com>
2020-12-01 23:39:07 -0600

    Resolving form functionality and some more form styling

--- assets/scripts/routes/contact.js
+++ assets/scripts/routes/contact.js
@@ -1,30 +1,68 @@
 import appState from '../util/appState';
+import autosize from 'autosize';
 
 const contact = {
   init() {
+    // JavaScript to be fired on the contact page
+
+    // Contact form
+    let $form = $('#contact-form');
+
+    // Contact message autoexpands
+    let contactText = document.querySelector('#contact-form textarea');
+    if (contactText) {
+      autosize(contactText);
+    }
+
+    // Contact submit button changes text on hover
+    let submitButton = document.querySelector('.contact-form-wrap button[type=submit]');
+    let submitButtonSpan = submitButton.querySelector('span');
+
     // AJAXify contact form submission
-    let form = $('#contact-form');
-    let formResponse = document.querySelector('#page-contact .form-response');
-    let formWrap = document.querySelector('#page-contact .form-wrap');
-    form.submit(function(e) {
+    let formResponse = document.querySelector('.contact-form-wrap .form-response');
+    let formWrap = document.querySelector('.contact-form-wrap .form-wrap');
+
+    // Store initial thanks copy if replaced by error
+    let thanksHtml = formResponse.innerHTML;
+
+    $form.on('submit', (e) => {
       e.preventDefault();
+      if (appState.requestInProgress) {
+        return false;
+      }
+      let $subject = $form.find('input[name=subject]');
+      let fromName = $form.find('input[name=fromName]').val();
+
+      // Set dynamic subject line to avoid gmail making a conversation thread and hiding text
+      $subject.val($subject.data('original-value') + ' from ' + fromName);
+
+      // Set appState flag to avoid multiple submits
+      appState.requestInProgress = true;
+
+      $form.addClass('working');
+      submitButtonSpan.textContent = 'Working...';
+
+      // Submit form
       $.ajax({
-        url: form.attr('action'),
-        type: form.attr('method'),
-        dataType: 'html',
-        data: form.serialize(),
-        success: (result) => {
-          formResponse.innerHTML = result;
-          if (result.match('success')) {
+        url: '/',
+        'type': 'POST',
+        dataType: 'json',
+        data: $form.serialize(),
+        success: (response) => {
+          if (response.success) {
+            formResponse.innerHTML = thanksHtml;
             formWrap.classList.add('-success');
+          } else {
+            formResponse.innerHTML = response.error || '<p>There was an error, please refresh and <a href="/contact">try again</a>.</p>';
           }
-          // zenscroll.center(formResponse);
-        },
-        error: (result) => {
-          formWrap.classList.add('-error');
-          formResponse.innerHTML = '<p>There was an error, please try again.</p>';
-          // zenscroll.center(formResponse);
         }
+      }).fail(() => {
+        formResponse.innerHTML = '<p>There was an error, please refresh and <a href="/contact">try again</a>.</p>';
+      }).always(() => {
+        $form.removeClass('working');
+        submitButtonSpan.textContent = 'Submit';
+        formResponse.classList.add('-active');
+        appState.requestInProgress = false;
       });
     });
 
